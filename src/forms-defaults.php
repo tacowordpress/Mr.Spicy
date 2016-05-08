34a99efbc64959d98af13f8e8a5195ce45971b7b
Jasand <jasandpereza@gmail.com>
2016-05-07 19:19:22 -0600

    correct path

--- src/forms-defaults.php
+++ src/forms-defaults.php
@@ -6,7 +6,7 @@
  *   in the form config (hardcoded), or if the admin adds values in the wp-admin (if applicable).
  */
 return array(
-  'form_action' => '/wp-content/themes/taco-theme/app/core/vendor/tacowordpress/MrSpicy/src/core/FormSubmit.php',
+  'form_action' => '/wp-content/themes/taco-theme/app/core/vendor/tacowordpress/mr-spicy/src/core/FormSubmit.php',
   'error_message' => 'There were some errors with your form submission. Please correct and try again.',
   'success_message' => 'Thanks for your message',
 );
