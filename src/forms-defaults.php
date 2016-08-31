21e6c465669cbc907599545c7a680f9d08f9ab20
Jasand Pereza <jasandpereza@gmail.com>
2016-08-31 15:30:38 -0600

    Update forms-defaults.php

--- src/forms-defaults.php
+++ src/forms-defaults.php
@@ -5,6 +5,8 @@
  *   in the form config (hardcoded), or if the admin adds values in the wp-admin (if applicable).
  */
 return array(
+  // The element "form_action" must be changed if you're using this file without the Taco Boilerplate
+  //TODO: Add WordPress rewrite (non .htaccess) for this path so the user doesn't have to specify it
   'form_action' => '/wp-content/themes/taco-theme/app/core/vendor/tacowordpress/mr-spicy/src/core/FormSubmit.php',
   'error_message' => 'There were some errors with your form submission. Please correct and try again.',
   'success_message' => 'Thanks for your message',
