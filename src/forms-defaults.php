5186262227fbd450d7dedce15e0d04651b038851
Jasand <jasandpereza@gmail.com>
2016-08-31 15:10:58 -0600

    change comments for TODO

--- src/forms-defaults.php
+++ src/forms-defaults.php
@@ -1,7 +1,6 @@
 <?php
 
-/* TODO: Figure out how to allow customization of this outside of the composer vendor directory
- * This file allows defaults to be assigned globally for all Mr. Spicy forms.
+/* This file allows defaults to be assigned globally for all Mr. Spicy forms.
  * These defaults will be overridden if the developer specifies these values
  *   in the form config (hardcoded), or if the admin adds values in the wp-admin (if applicable).
  */
