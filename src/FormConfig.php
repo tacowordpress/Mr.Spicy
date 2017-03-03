ca669a02d928ba40f30f2280ee3daf4dd79eeb12
Jasand Pereza <jasand@jasands-MacBook-Pro.local>
2017-03-03 09:25:04 -0700

    not proud of this

--- src/FormConfig.php
+++ src/FormConfig.php
@@ -18,5 +18,12 @@ if(!class_exists('FormConfig')) {
     public function excludeFromSearch() {
       return true;
     }
+
+     public function save($exclude_post=false) {
+       // I know. This is bad.
+      $fields = unserialize(unserialize(stripslashes($this->get('fields'))));
+      $this->set('fields', serialize($fields));
+      return parent::save($exclude_post);
+    }
   }
 }
