428b462ffc2c652f9340d9eb1e6eda9cf2894c8a
Ian Zhang <ianzhang@vermilion.com>
2017-07-07 10:59:38 -0600

    Adding recursive unserialize

--- src/FormConfig.php
+++ src/FormConfig.php
@@ -20,8 +20,17 @@ if(!class_exists('FormConfig')) {
     }
 
      public function save($exclude_post=false) {
-       // I know. This is bad.
-      $fields = unserialize(unserialize(stripslashes($this->get('fields'))));
+       // I know. This is bad. But not as bad anymore.
+       $fields = stripslashes($this->get('fields'));
+       while(!is_array($fields)) {
+         $fields = unserialize($fields);
+
+         if (!$fields) {
+           $fields = [];
+           break;
+         }
+       }
+
       $this->set('fields', serialize($fields));
       return parent::save($exclude_post);
     }
